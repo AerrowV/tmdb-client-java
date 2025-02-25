214524e0d9d511bcd51c3d9625100808392e1240
AerrowV <businessasimk@gmail.com>
2025-02-25 10:19:01 +0100

    Initial commit

---
+++ src/main/java/dat/exceptions/ApiException.java
@@ -0,0 +1,14 @@
+package dat.exceptions;
+
+public class ApiException extends RuntimeException {
+
+  private int errorCode;
+
+  public ApiException(int errorCode, String message) {
+    super(message);
+    this.errorCode = errorCode;
+  }
+  public int getErrorCode() {
+    return errorCode;
+  }
+}
