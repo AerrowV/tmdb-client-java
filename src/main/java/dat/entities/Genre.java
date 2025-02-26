06a50064833c0e739e8698e3363fcc6aea7d329c
Maximitas <Lassetimm@outlook.dk>
2025-02-26 11:42:57 +0100

    Relations på genre og movie.

--- src/main/java/dat/entities/Genre.java
+++ src/main/java/dat/entities/Genre.java
@@ -1,4 +1,24 @@
 package dat.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@ToString
+@Entity
 public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @ManyToMany(mappedBy = "genres", cascade = CascadeType.ALL) // Corrected field name
+    private Set<Movie> movies = new HashSet<>();
+
 }
--- src/main/java/dat/entities/Movie.java
+++ src/main/java/dat/entities/Movie.java
@@ -1,5 +1,30 @@
 package dat.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@ToString
+@Entity
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "movie_genre",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id")
+    )
+    private Set<Genre> genres = new HashSet<>();
 
 }
