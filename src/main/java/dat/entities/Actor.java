713accb1de667693dbde7fc1946366665ee5b2cf
Jonas <glemdeet@gmail.com>
2025-02-26 11:58:30 +0100

    DTO push

--- src/main/java/dat/entities/Actor.java
+++ src/main/java/dat/entities/Actor.java
@@ -1,4 +1,18 @@
 package dat.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Set;
+
+@Entity
+@Data
 public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+
+    @ManyToMany(mappedBy = "actors")
+    private Set<Movie> movies;
 }
